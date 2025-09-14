4a167223bad2162d33b503122af89036b78c5ac5
TranVanHuy2603 <tranvanhuy@msi-cua-huy.lan>
2025-09-14 16:50:19 +0700

    Test Linux

--- scr/Entity/Character.cpp
+++ scr/Entity/Character.cpp
@@ -2,6 +2,8 @@
 #include "Entity.h"
 #include <SFML/Graphics.hpp>
 
+sf::Texture Character::texture; 
+
 Character::Character(int x, int y, double radius, bool walkable, 
           int hp, int hp_max, int damage, 
           double damage_range, double attack_speed, int gold, int exp)
