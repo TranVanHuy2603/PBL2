4e762f218fad5768d6c1edf83b0acb892a36cfbb
tranvanhuy <tranvanhuy064206@gmail.com>
2025-09-27 19:08:20 +0700

    update 27/09

--- scr/main.cpp
+++ scr/main.cpp
@@ -8,7 +8,22 @@ int main()
     sf::RenderWindow window(sf::VideoMode(800, 600), "PBL2");
     window.setFramerateLimit(60);
 
-    EntityManager manager;
+    Rect worldRect(0, 0, 800, 600); //khoi tao vung ban do cho quadtree
+    EntityManager manager(worldRect, 10); //quan li entity
+
+    Character* player = new Character(100, 100, false, 100, 100, 1, 0, 0, 20); //khoi tao nhan vat
+    manager.set_player(player);
+    manager.add(player);
+
+    Castle* castle = new Castle(200, 200, false, 500, 500, 1, 20);
+    manager.set_castle(castle);
+    manager.add(castle);
+
+    for (int i = 0; i < 15; i++)
+    {
+        Monster* m = new Monster(50 + i * 100, 50, false, 50, 50, 5, 60, rand()*10, 10, 20); 
+        manager.add(m);
+    }
 
     while (window.isOpen())
     {
